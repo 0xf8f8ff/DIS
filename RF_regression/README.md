8b4f0df995be83e39ce4e2a608fccf491f91acef
0xf8f8ff <0xf8f8ff@protonmail.com>
2021-04-24 20:39:03 +0200

    Working, indexing a bit off, to be fixed

--- RF_regression/README.md
+++ RF_regression/README.md
@@ -6,6 +6,11 @@ Additional preprocessing specifically for RF algo:
 
 3. We drop columns with index and location name, otherwise the algorithm would treat them as possible features slowing down the performance. 
 
-4. We transform the date column into one-hot encoded seasons to use as a parameter in our RFR model.
+4. We transform the date column into month number.
+
+5. Additionally, we split the dataset into the training and testing subsets. All data for 2020 goes into training set, all data for 2021 - into testing.
 
 
+```
+python optimized.py --files rf_testing.csv,rf_training.csv --hadoop-streaming-jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -r hadoop hdfs:///testdata/dummy.csv
+```
--- RF_regression/rf_mr.py
+++ RF_regression/rf_mr.py
@@ -1,14 +1,9 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 import math
 import numpy as np
-
-class TreeNode:
-    branches: list # list of children
-    feature: int
-    split: float
-    leaf_value: float # final value
-
+import pandas as pd
+import datetime
+import sys
 
 class RFTree:
     def __init__(self, training, testing, depth, min_samples, sample_features):
@@ -18,165 +13,179 @@ class RFTree:
         self.min_samples = min_samples
         self.sample_features = sample_features
 
-    def model(self):
-        root = self.split(self.training_set)
-        self.branch_out(root, 1)
+    def train(self):
+        print("TYPE OF TRAINING SET: ", type(self.training_set))
+        root = self.__split(self.training_set)
+        print("Type of root: ", type(root))
+        self.__branch_out(root, 1)
         return root
 
     # define split conditions for each feature, measure importance
     # return a new internal node with condition and branches
-    def split(self, tree_node):
-        # choose n features
-        random_features = np.random.choice(self.training_set.shape[0]-1, self.sample_features)
-        # split on each {i: branches}
-        candidates = {}
-        for i in random_features:
-            condition = np.mean(self.training_set[:, i].astype('float'))
-            left_branch = []
-            right_branch = []
-            for record in tree_node:
-                if record[i] <= condition:
-                    left_branch.append(record)
+    def __split(self, tree_node_records):
+        if isinstance(tree_node_records, list) or isinstance(tree_node_records, np.ndarray):
+            print("RIGHT TYPE, MAKING NODE: ", type(tree_node_records))
+            # choose n features (without replacement)
+            print("Total feature columns: ", self.training_set.shape[1] - 1)
+            random_features = np.random.choice(np.arange(0, self.training_set.shape[1]-1), self.sample_features, replace=False)
+    #        print("Features chosen: " + str(random_features))
+            # split on each {i: branches}
+            candidates = {}
+            for i in random_features:
+                condition = np.nanmean(np.array(tree_node_records)[:, i].astype('float'))
+    #            print("Feature mean for feature " + str(i) + ": " + str(condition))
+                left_branch = []
+                right_branch = []
+                for record in tree_node_records:
+                    if record[i] < condition:
+                        left_branch.append(record)
+                    else:
+                        right_branch.append(record)
+    #            print("Branches len: " + str([len(left_branch), len(right_branch)]))
+                feature_list = list(np.array(tree_node_records)[:, i].astype('float'))
+                count = feature_list.count(condition)
+                if count == len(feature_list) or len(left_branch) == 0 or len(right_branch) == 0:
+                    print("Feature ", i, " has only one value: ", condition)
                 else:
-                    right_branch.append(record)
-            candidates[i] = [left_branch, right_branch]
-
-        # compare importance and choose one
-        sds = {}
-        for k , v in candidates:
-            sds[k] = self.get_feature_importance(v)
-        chosen_feature = min(sds.items(), key=lambda x: x[1])
-        tree_node["branches"] = candidates[chosen_feature[0]]
-        tree_node["feature"] = chosen_feature[0] # index of the chosen feature
-        tree_node["condition"] = np.mean(self.training_set[:, chosen_feature[0]].astype('float'))
-
-    def branch_out(self, tree_node, depths):
-        # shape = 2 for all features except the seasonal (where it is 4)
-        for branch in tree_node["branches"]:
-            if depths >= self.tree_depth or branch.shape[0] <= self.min_samples:
-                # take mean value of results (y) for this leaf, converting to floats first
-                branch = np.mean(branch[:, -1].astype('float'))
-            # else make a node and branch out on a condition
+                    candidates[i] = list([left_branch, right_branch])
+    #        print("Candidates chosen: " + str(candidates.keys()))
+            if len(candidates.items()) == 0:
+                # make a leaf node
+                new_node = np.nanmean(np.array(tree_node_records)[:, -1].astype('float'))
+                print("No candidates, LEAF NODE: ", type(new_node))
+                return new_node
             else:
-                branch = self.split(branch)
-                self.branch_out(branch, depths+1)
-    
-    def get_feature_importance(self, branches):
+                # compare importance and choose one
+                sds = {}
+                for k , v in candidates.items():
+                    sds[k] = self.__get_feature_importance(v)
+                chosen_feature = min(sds.items(), key=lambda x: x[1])
+    #            print("CHOSEN FEATURE: " + str(chosen_feature[0]))
+                new_node = {}
+                branches = {}
+                branches["left"] = candidates[chosen_feature[0]][0]
+                branches["right"] = candidates[chosen_feature[0]][1]
+                new_node["branches"] = branches
+                new_node["feature"] = chosen_feature[0] # index of the chosen feature
+                new_node["condition"] = np.nanmean(np.array(tree_node_records)[:, chosen_feature[0]].astype('float'))
+                return new_node
+        else:
+            print("WRONG TYPE OF NODE: ", type(tree_node_records))
+
+    def __branch_out(self, tree_node, depths):
+        
+        print("Branching out, depth: " + str(depths))
+        print("Type of tree_node: ", type(tree_node))
+        if isinstance(tree_node, dict):
+            for side, branch in tree_node["branches"].items():
+                #print("BRANCH: ", branch)
+                if depths >= self.tree_depth or np.array(branch).shape[0] <= self.min_samples:
+                    
+                    # take mean value of results (y) for this leaf, converting to floats first
+                    branch = np.nanmean(np.array(branch)[:, -1].astype('float'))
+                    tree_node[side] = branch
+                    print("LEAF NODE: ", branch)
+                # else make a node and branch out on a condition
+                else:
+                    branch = self.__split(branch)
+                    self.__branch_out(branch, depths+1)
+                    tree_node[side] = branch
+
+        elif isinstance(tree_node, list):
+            print("NODE IS LIST OF LEN ", len(tree_node))
+        
+    def __get_feature_importance(self, branches):
         # POOLED STANDARD DEVIATION
         if len(branches) != 2:
-            return 0
-        sd1 = np.std(branches[0][:,-1])
-        sd2 = np.std(branches[1][:,-1])
+            return 666
+        sd1, sd2 = (0, 0)
+        if len(branches[0]) > 0:
+            left = np.array(branches[0])[:,-1]
+            sd1 = np.std(left)
+        if len(branches[1]) > 0:
+            right = np.array(branches[1])[:,-1]
+            sd2 = np.std(right)
+
         pooled = math.sqrt((math.sqrt(sd1) + math.sqrt(sd2))/2)
         return pooled
 
 
-    def train(self, tree_node, datarow):
-        # TODO: split based on attributes
-        # split further if not leaf node
-        pass
-
-    def predict(self, tree):
+    def __predict(self, tree_node, datarow):
+        print("Predicting, type of tree_node: ", type(tree_node))
+        print("Left type: ", type(tree_node["left"]))
+        print("Right type: ", type(tree_node["right"]))
+        #if isinstance(tree_node, dict):
+        if datarow[tree_node["feature"]] < tree_node["condition"]:
+            if isinstance(tree_node["left"], dict):
+                return self.__predict(tree_node["left"], datarow)
+            else:
+                print("PREDICTING: ", tree_node["left"])
+                return tree_node["left"] # return left branch as prediction
+                
+        else:
+            if isinstance(tree_node["right"], dict):
+                return self.__predict(tree_node["right"], datarow)
+            else:
+                print("PREDICTING: ", tree_node["right"])
+                return tree_node["right"] # return right value
+    
+    def get_predictions(self, tree):
         test = self.testing_set
-        predicted_values = []
+        predicted_values = list()
         for row in test:
-            predicted = self.train(tree, row)
-            date_real_predicted = list(row[0], row[-1], predicted)
-            predicted_values.append(date_real_predicted)
+            oldrow = row
+            #print("Row before: ", len(oldrow))
+            datarow = row[3:-1]
+            #print("Row after: ", len(datarow))
+            predicted = self.__predict(tree, datarow)
+            print("Predicted value: ", predicted)
+            print("Real value: ", oldrow[-1])
+            #date_real_predicted = list(row[0], predicted)
+            #predicted_values.append(date_real_predicted)
+            predicted_values.append(predicted)
         return predicted_values
 
-class MRRandomForestRegression(MRJob):
+
+class MRRFR(MRJob):
     def configure_args(self):
-        super(MRRandomForestRegression, self).configure_args()
-        self.add_passthru_arg('--tree_nr', type=int, default=100, help='Number of decision trees to build, default - 100')
+        super(MRRFR, self).configure_args()
+        #self.add_passthru_arg('--tree_nr', type=int, default=100, help='Number of decision trees to build, default - 100')
         self.add_passthru_arg('--tree_depth', type=int, default=10, help='Max depth of decision trees, defaut - 10')
         self.add_passthru_arg('--min_samples', type=int, default=10, help='Max number of values after a split to make a leaf node')
         self.add_passthru_arg('--location', type=str, default="Norway", help='Country name, default - Norway')
+        # TODO: add option to process all countries
 
-    def steps(self):
-        return [
-            MRStep(mapper_init = self.mapper_init_preprocess,
-            mapper = self.mapper_preprocess),
-            MRStep(mapper_init = self.mapper_init_model,
-            reducer = self.reducer_model)
-        ]
 
-    def mapper_init_preprocess(self):
-        header_names = ["index","location","date","total_cases","new_cases","new_cases_smoothed","total_deaths","new_deaths","new_deaths_smoothed","total_cases_per_million","new_cases_smoothed_per_million","total_deaths_per_million","new_deaths_smoothed_per_million","reproduction_rate","total_tests","total_vaccinations","people_vaccinated","people_fully_vaccinated","new_vaccinations","new_vaccinations_smoothed","total_vaccinations_per_hundred","people_vaccinated_per_hundred","people_fully_vaccinated_per_hundred","new_vaccinations_smoothed_per_million","stringency_index","population","population_density","median_age","aged_65_older","aged_70_older","gdp_per_capita","extreme_poverty","cardiovasc_death_rate","diabetes_prevalence","female_smokers","male_smokers","hospital_beds_per_thousand","life_expectancy","human_development_index","new_tests","days_after_100_cases","internal_flights","international_arrivals"]
-        # make dict [header]index for quick lookup
-        self.headers = {}
-        for i, h in header_names:
-            self.headers[h] = i
+    def mapper_init(self):
+        self.training = pd.read_csv("rf_training.csv")
+        self.testing = pd.read_csv("rf_testing.csv")
 
-        # one-hot encode date as season
-        #season_names = pd.Series(["winter", "spring", "summer", "autumn"])
-        # winter = [1,0,0,0]
-        # spring = [0,1,0,0]
-        # summer = [0,0,1,0]
-        # autumn = [0,0,0,1]
-        
-        # yearSeasons = [
-        #     winter, winter, spring,
-        #     spring, spring, summer,
-        #     summer, summer, autumn,
-        #     autumn, autumn, winter,
-        # ]
-
-        # self.seasons = dict(zip(range(1,13), yearSeasons))
-
-        # initialize structures for training and testing subsets 
-        self.training = []
-        self.testing = []
-                
-    def mapper_preprocess(self, _, line):
-        row = line.split(",")
-        # ignore malformed rows, only consider rows for the chosen country
-        if len(row) == len(self.headers) and row[1] == self.options.location:
-            # choose only independent features
-            new_record = [row["date"], row["total_cases_per_million"], row["total_deaths_per_million"], row["reproduction_rate"], row["stringency_index"], row["population_density"], row["gdp_per_capita"], row["median_age"], row["human_development_index"], row["hospital_beds_per_thousand"], row["diabetes_prevalence"], row["days_after_100_cases"], row["internal_flights"], row["international_arrivals"], row["new_cases"]]
-            # replace all NaN values with zeroes
-            new_record = np.nan_to_num(new_record)
-            # replace date string with month number
-            #new_record = list(new_record)
-            date = new_record[0]
-            parts = date.split("-")
-            if len(parts) == 3:
-                # make a column with month numbers
-                encoded_date = parts[1] #self.seasons[int(parts[1])]
-                # for training data (year 2020) append the column with new cases
-                # then add column with one-hot encoded season
-                new_record.insert(1, encoded_date)
-                if parts[0] == "2021":
-                    self.testing.append(list(new_record))
-                else:
-                    self.training.append(list(new_record))
+    def mapper(self, _, next_tree):
+        records_total = self.training.shape[0]
+        features_total = self.training.shape[1] - 1
+        sample_features = int(math.floor(math.sqrt(features_total)))
+        # pick sample records with replacement
+        samples = np.random.choice(records_total, records_total)
+        tree_samples = self.training.ix[samples]
+        tree = RFTree(training = tree_samples, testing = self.testing, depth = self.options.tree_depth, min_samples = self.options.min_samples, sample_features = sample_features)
+        model = tree.train()
+        predicted = tree.get_predictions(model)
+        yield None, predicted
     
+    def reducer(self, key, values):
+        date_column = self.testing[:, 2]
+        location_column = self.testing[:, 1]
 
-    def mapper_init_model(self):
-        self.total_features = self.training_data.shape[1]
-        self.sample_features = math.floor(math.sqrt(self.total_features))
+        combined_predictions = np.array(list(values))
+        if len(date_column) != combined_predictions.shape[0]:
+            yield None, "LENGTH MISMATCH: " + len(date_column) + " vs " + combined_predictions.shape[0]
+        else:
+            result = np.mean(combined_predictions, axis=1)
+            with_date = np.vstack(location_column, date_column, result)
+            yield None, with_date
 
-    def mapper_model(self, _):
-        # TODO: leave or remove the raw date column?
-        training_data = np.array(self.training)#[1:]
-        length = training_data.shape[0]
-
-        # bootstrap-sample trees with replacement
-        for i in range(1, self.options.trees):
-            # pick random 4 features for the tree
-            #cols = np.random.choice(self.total_features, self.sample_features)
-            #tree_data = training_data[:, cols]
-            # pick sample records with replacement
-            samples = np.random.choice(length, length)
-            tree_samples = training_data[samples]
-            tree = RFTree(training = tree_samples, testing = self.testing, depth = self.options.tree_depth, min_samples = self.options.min_samples, sample_features = self.sample_features)
-            tree_model = tree.model()
-            predicted = tree.predict(tree_model)
-            yield None, predicted
-
-    def reducer_model(self, _, values):
-        # TODO: sort values by date
-
-        # TODO: take mean for each date as final prediction
-        all_predictions = np.array(list(values))
-        # TODO (MAYBE) transform into rows [date, all predictions for this date] - needs an extra combiner?
+if __name__ == '__main__':
+    start = datetime.datetime.now()
+    MRRFR.run()
+    stop = datetime.datetime.now()
+    sys.stderr.write("RUNNING TIME: " + str(stop - start))
